1cf9bf08407ee17bcc0075c834e144f5474ce736
Dongkuk Kim <dongkukkim@Dongkukui-MacBookPro.local>
2025-03-13 18:59:59 +0900

    chore: remove trivago/prettier-plugin-sort-imports

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -3,19 +3,17 @@ import CommonButton from 'components/button/commonButton';
 
 const HomeScreen: React.FC = () => {
   return (
-    <SafeAreaView
-      style={styles.backgroundContainer}
-    >
-        <Text>Home Screen</Text>
-        <CommonButton></CommonButton>
+    <SafeAreaView style={styles.backgroundContainer}>
+      <Text>Home Screen</Text>
+      <CommonButton></CommonButton>
     </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
-  backgroundContainer:{
-    flex:1
-  }
+  backgroundContainer: {
+    flex: 1,
+  },
 });
 
 export default HomeScreen;
