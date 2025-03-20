96cd7ca9d92e39ec5e7d419f39a924e2bec75aa9
Dongkuk Kim <dongkukkim@Dongkukui-MacBookPro.local>
2025-03-20 19:08:53 +0900

    chore: add react-native-gesture-handler, react-native-reanimated

--- App.tsx
+++ App.tsx
@@ -1,17 +1,20 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import { HomeScreen } from './src/screens';
+import {GestureHandlerRootView} from 'react-native-gesture-handler';
+import {HomeScreen} from './src/screens';
 
 const Stack = createNativeStackNavigator();
 
 function RootStack() {
   return (
-    <Stack.Navigator
-      initialRouteName="Home"
-      screenOptions={{headerShown: false}}>
-      <Stack.Screen name="Home" component={HomeScreen} />
-    </Stack.Navigator>
+    <GestureHandlerRootView>
+      <Stack.Navigator
+        initialRouteName="Home"
+        screenOptions={{headerShown: false}}>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </GestureHandlerRootView>
   );
 }
 
--- babel.config.js
+++ babel.config.js
@@ -13,6 +13,7 @@ module.exports = {
           assets: './src/assets',
         },
       },
-    ]
+    ],
+    'react-native-reanimated/plugin',
   ],
 };
