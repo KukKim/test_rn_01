ea8d598e1e695c621f382980fe7be0cb71e1eeb4
Dongkuk Kim <dongkukkim@Dongkukui-MacBookPro.local>
2025-04-04 17:46:39 +0900

    feat: add react-native-gesture-handler example

--- src/screens/test/test_gestureHandler.tsx
+++ src/screens/test/test_gestureHandler.tsx
@@ -1,16 +1,85 @@
-import {SafeAreaView, Text, StyleSheet, FlatList} from 'react-native';
+import {SafeAreaView, View, Text, StyleSheet} from 'react-native';
+import {Gesture, GestureDetector} from 'react-native-gesture-handler';
+import Animated, {
+  useSharedValue,
+  useAnimatedStyle,
+  withSpring,
+} from 'react-native-reanimated';
 
 const TestGestureHandlerScreen: React.FC = () => {
   return (
     <SafeAreaView style={styles.backgroundContainer}>
       <Text>Test Gesture Handler Screen</Text>
+
+      <View
+        style={{
+          borderWidth: 1,
+          width: 50,
+          height: 50,
+        }}
+        // onStartShouldSetResponder={p => {
+        //   console.log('??');
+        //   console.log(p);
+        // }}
+      >
+        <Text>{'Test component'}</Text>
+        <Ball />
+      </View>
     </SafeAreaView>
   );
 };
 
+function Ball() {
+  const isPressed = useSharedValue(false);
+  const offset = useSharedValue({x: 0, y: 0});
+  const start = useSharedValue({x: 0, y: 0});
+  const gesture = Gesture.Pan()
+    .onBegin(() => {
+      isPressed.value = true;
+    })
+    .onUpdate(e => {
+      offset.value = {
+        x: e.translationX + start.value.x,
+        y: e.translationY + start.value.y,
+      };
+    })
+    .onEnd(() => {
+      start.value = {
+        x: offset.value.x,
+        y: offset.value.y,
+      };
+    })
+    .onFinalize(() => {
+      isPressed.value = false;
+    });
+
+  const animatedStyles = useAnimatedStyle(() => {
+    return {
+      transform: [
+        {translateX: offset.value.x},
+        {translateY: offset.value.y},
+        {scale: withSpring(isPressed.value ? 1.2 : 1)},
+      ],
+      backgroundColor: isPressed.value ? 'yellow' : 'blue',
+    };
+  });
+  return (
+    <GestureDetector gesture={gesture}>
+      <Animated.View style={[styles.ball, animatedStyles]} />
+    </GestureDetector>
+  );
+}
+
 const styles = StyleSheet.create({
   backgroundContainer: {
     flex: 1,
+  },
+  ball: {
+    width: 100,
+    height: 100,
+    borderRadius: 100,
+    backgroundColor: 'blue',
+    alignSelf: 'center',
   },
 });
 
